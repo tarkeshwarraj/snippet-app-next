60c209701f7469714d84594f2f1598c10a086cc3
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-02-18 20:07:19 +0530

    page update

--- src/app/greeting/[name]/page.js
+++ src/app/greeting/[name]/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = ({params}) => {
   return (
     <div>
-        <h1>Nice to meet you. {params.name}</h1>
+        <h1>Nice to meet you {params.name}</h1>
     </div>
   )
 }
